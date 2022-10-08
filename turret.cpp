c8ff5a7b838be6961e7c765354aa15169d8c81d7
Alistair Keiller <alistair@keiller.net>
2022-10-07 19:16:09 -0700

    test

--- turret.cpp
+++ turret.cpp
@@ -4,42 +4,42 @@
 // #define targetHeight 5.0; // feet
 const double targetHeight = 5.0; // feet WTF change to define later
 
-class threeVector
+class Vec3
 {
 public:
-    double x, y, z;
-    threeVector(double x, double y, double z)
-    {
-        this->x = x;
-        this->y = y;
-        this->z = z;
-    }
-    double distance(threeVector other)
-    {
-        return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2) + pow(z - other.z, 2));
-    }
-    void add(threeVector other)
-    {
-        x += other.x;
-        y += other.y;
-        z += other.z;
-    }
+  double x, y, z;
+  Vec3(double x, double y, double z)
+  {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+  }
+  double distance(Vec3 other)
+  {
+    return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2) +
+                pow(z - other.z, 2));
+  }
+  double magnitude() { return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)); }
+  void add(Vec3 other)
+  {
+    x += other.x;
+    y += other.y;
+    z += other.z;
+  }
 };
 
-int main()
-{
-    return 0;
-}
+int main() { return 0; }
 
-int calculateTrajectory(threeVector pos, threeVector vel)
+int calculateTrajectory(Vec3 pos, Vec3 vel)
 {
 }
 
 int calcutareAirTime(double distance)
 {
-    return sqrt((tan(shooterAngle) * distance - targetHeight) / 4.9);
+  return sqrt((tan(shooterAngle) * distance - targetHeight) / 4.9);
 }
 
-int calculateExitVelocity(double distance)
+double calculateExitVelocity(Vec3 vel, double distance, int airtime)
 {
+  return (distance / airtime - vel.magnitude()) / cos(shooterAngle);
 }
