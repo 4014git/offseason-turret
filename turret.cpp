84b821c9ffac081337596a0d83b7b2b1328bda8c
Alistair Keiller <alistair@keiller.net>
2022-10-07 20:53:22 -0700

    draft

--- turret.cpp
+++ turret.cpp
@@ -1,37 +1,22 @@
+#include <utility>
 #include <math.h>
 #define PI 3.14159265
 #define shooterAngle PI / 4
-// #define targetHeight 5.0; // feet
-const double targetHeight = 5.0; // feet WTF change to define later
+#define targetHeight 5.0 // feet TODO: set this to real
 
-class Vec3
+int magnitude(std::pair<double, double> p)
 {
-public:
-  double x, y, z;
-  Vec3(double x, double y, double z)
-  {
-    this->x = x;
-    this->y = y;
-    this->z = z;
-  }
-  double distance(Vec3 other)
-  {
-    return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2) +
-                pow(z - other.z, 2));
-  }
-  double magnitude() { return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)); }
-  void add(Vec3 other)
-  {
-    x += other.x;
-    y += other.y;
-    z += other.z;
-  }
-};
+  return sqrt(p.first * p.first + p.second * p.second);
+}
 
-int main() { return 0; }
-
-int calculateTrajectory(Vec3 pos, Vec3 vel)
+int main()
 {
+}
+
+double calculateYawAngle(std::pair<double, double> goal, std::pair<double, double> goalVel)
+{
+  std::pair<double, double> goalFuturePosition = std::make_pair(goal.first + goalVel.first * calcutareAirTime(magnitude(goal)), goal.second + goalVel.second * calcutareAirTime(magnitude(goal)));
+  return std::atan2(goalFuturePosition.first, goalFuturePosition.second);
 }
 
 int calcutareAirTime(double distance)
@@ -39,7 +24,7 @@ int calcutareAirTime(double distance)
   return sqrt((tan(shooterAngle) * distance - targetHeight) / 4.9);
 }
 
-double calculateExitVelocity(Vec3 vel, double distance, int airtime)
+double calculateExitVelocity(std::pair<double, double> vel, double distance, int airtime)
 {
-  return (distance / airtime - vel.magnitude()) / cos(shooterAngle);
+  return (distance / airtime - magnitude(vel)) / cos(shooterAngle);
 }
