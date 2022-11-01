4012cd8defcdfa9c4aaa699db894ab2fbd552898
Alistair Keiller <alistair@keiller.net>
2022-11-01 14:00:20 -0700

    add defines

--- auto.cpp
+++ auto.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 #include <autodiff/forward/dual.hpp>
 using namespace autodiff;
 
@@ -12,6 +13,41 @@ using namespace autodiff;
 #define acceptableError 1e-2 // m
 #define learningRate 1e-2 // m^-1
 
+#define dx(vx) (vx)
+#define dy(vy) (vy)
+#define dz(vz) (vz)
+#define dvx(vx, vy, vz) (μ*(vx)*sqrt((vx)*(vx)+(vy)*(vy)+(vz)*(vz)))
+#define dvy(vx, vy, vz) (μ*(vy)*sqrt((vx)*(vx)+(vy)*(vy)+(vz)*(vz)))
+#define dvz(vx, vy, vz) (μ*(vz)*sqrt((vx)*(vx)+(vy)*(vy)+(vz)*(vz)) + g)
+
+// #define k1x dx(vx)
+// #define k1y dy(vy)
+// #define k1z dz(vz)
+// #define k1vx dvx(vx, vy, vz)
+// #define k1vy dvy(vx, vy, vz)
+// #define k1vz dvz(vx, vy, vz)
+
+// #define k2x dx(vx+dt*k1vx/2)
+// #define k2y dy(vy+dt*k1vy/2)
+// // #define k2z dz(vz+dt*k1vz/2)
+// #define k2vx dvx(vx+dt*k1vx/2, vy+dt*k1vy/2, vz+dt*k1vz/2)
+// #define k2vy dvy(vx+dt*k1vx/2, vy+dt*k1vy/2, vz+dt*k1vz/2)
+// #define k2vz dvz(vx+dt*k1vx/2, vy+dt*k1vy/2, vz+dt*k1vz/2)
+
+// #define k3x dx(vx+dt*k2vx/2)
+// #define k3y dy(vy+dt*k2vy/2)
+// #define k3z dz(vz+dt*k2vz/2)
+// #define k3vx dvx(vx+dt*k2vx/2, vy+dt*k2vy/2, vz+dt*k2vz/2)
+// #define k3vy dvy(vx+dt*k2vx/2, vy+dt*k2vy/2, vz+dt*k2vz/2)
+// #define k3vz dvz(vx+dt*k2vx/2, vy+dt*k2vy/2, vz+dt*k2vz/2)
+
+// #define k4x dx(vx+dt*k3vx)
+// #define k4y dy(vy+dt*k3vy)
+// #define k4z dz(vz+dt*k3vz)
+// #define k4vx dvx(vx+dt*k3vx, vy+dt*k3vy, vz+dt*k3vz)
+// #define k4vy dvy(vx+dt*k3vx, vy+dt*k3vy, vz+dt*k3vz)
+// #define k4vz dvz(vx+dt*k3vx, vy+dt*k3vy, vz+dt*k3vz)
+
 // https://en.wikipedia.org/wiki/Projectile_motion#Trajectory_of_a_projectile_with_Newton_drag
 // https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta_methods
 dual getShotError(const dual tx, const dual ty, const dual rvx, const dual rvy, const dual θ, const dual v)
@@ -21,33 +57,33 @@ dual getShotError(const dual tx, const dual ty, const dual rvx, const dual rvy, 
     vx = v*cos(θ)*cos(Φ) + rvx, vy = v*sin(θ)*cos(Φ) + rvy, vz = v*sin(Φ);
   while(vz > 0 || z > tz)
   {
-    dual k1x = vx;
-    dual k1y = vy;
-    dual k1z = vz;
-    dual k1vx = μ*vx*sqrt(vx*vx+vy*vy+vz*vz);
-    dual k1vy = μ*vy*sqrt(vx*vx+vy*vy+vz*vz);
-    dual k1vz = μ*vz*sqrt(vx*vx+vy*vy+vz*vz) + g;
+    dual k1x = dx(vx);
+    dual k1y = dy(vy);
+    dual k1z = dz(vz);
+    dual k1vx = dvx(vx, vy, vz);
+    dual k1vy = dvy(vx, vy, vz);
+    dual k1vz = dvz(vx, vy, vz);
+    
+    dual k2x = dx(vx+dt*k1vx/2);
+    dual k2y = dy(vy+dt*k1vy/2);
+    dual k2z = dz(vz+dt*k1vz/2);
+    dual k2vx = dvx(vx+dt*k1vx/2, vy+dt*k1vy/2, vz+dt*k1vz/2);
+    dual k2vy = dvy(vx+dt*k1vx/2, vy+dt*k1vy/2, vz+dt*k1vz/2);
+    dual k2vz = dvz(vx+dt*k1vx/2, vy+dt*k1vy/2, vz+dt*k1vz/2);
 
-    dual k2x = vx+dt*k1vx/2;
-    dual k2y = vy+dt*k1vy/2;
-    dual k2z = vz+dt*k1vz/2;
-    dual k2vx = μ*(vx+dt*k1vx/2)*sqrt((vx+dt*k1vx/2)*(vx+dt*k1vx/2)+(vy+dt*k1vy/2)*(vy+dt*k1vy/2)+(vz+dt*k1vz/2)*(vz+dt*k1vz/2));
-    dual k2vy = μ*(vy+dt*k1vy/2)*sqrt((vx+dt*k1vx/2)*(vx+dt*k1vx/2)+(vy+dt*k1vy/2)*(vy+dt*k1vy/2)+(vz+dt*k1vz/2)*(vz+dt*k1vz/2));
-    dual k2vz = μ*(vz+dt*k1vz/2)*sqrt((vx+dt*k1vx/2)*(vx+dt*k1vx/2)+(vy+dt*k1vy/2)*(vy+dt*k1vy/2)+(vz+dt*k1vz/2)*(vz+dt*k1vz/2)) + g;
+    dual k3x = dx(vx+dt*k2vx/2);
+    dual k3y = dy(vy+dt*k2vy/2);
+    dual k3z = dz(vz+dt*k2vz/2);
+    dual k3vx = dvx(vx+dt*k2vx/2, vy+dt*k2vy/2, vz+dt*k2vz/2);
+    dual k3vy = dvy(vx+dt*k2vx/2, vy+dt*k2vy/2, vz+dt*k2vz/2);
+    dual k3vz = dvz(vx+dt*k2vx/2, vy+dt*k2vy/2, vz+dt*k2vz/2);
 
-    dual k3x = vx+dt*k2vx/2;
-    dual k3y = vy+dt*k2vy/2;
-    dual k3z = vz+dt*k2vz/2;
-    dual k3vx = μ*(vx+dt*k2vx/2)*sqrt((vx+dt*k2vx/2)*(vx+dt*k2vx/2)+(vy+dt*k2vy/2)*(vy+dt*k2vy/2)+(vz+dt*k2vz/2)*(vz+dt*k2vz/2));
-    dual k3vy = μ*(vy+dt*k2vy/2)*sqrt((vx+dt*k2vx/2)*(vx+dt*k2vx/2)+(vy+dt*k2vy/2)*(vy+dt*k2vy/2)+(vz+dt*k2vz/2)*(vz+dt*k2vz/2));
-    dual k3vz = μ*(vz+dt*k2vz/2)*sqrt((vx+dt*k2vx/2)*(vx+dt*k2vx/2)+(vy+dt*k2vy/2)*(vy+dt*k2vy/2)+(vz+dt*k2vz/2)*(vz+dt*k2vz/2)) + g;
-
-    dual k4x = vx+dt*k3vx;
-    dual k4y = vy+dt*k3vy;
-    dual k4z = vz+dt*k3vz;
-    dual k4vx = μ*(vx+dt*k3vx)*sqrt((vx+dt*k3vx)*(vx+dt*k3vx)+(vy+dt*k3vy)*(vy+dt*k3vy)+(vz+dt*k3vz)*(vz+dt*k3vz));
-    dual k4vy = μ*(vy+dt*k3vy)*sqrt((vx+dt*k3vx)*(vx+dt*k3vx)+(vy+dt*k3vy)*(vy+dt*k3vy)+(vz+dt*k3vz)*(vz+dt*k3vz));
-    dual k4vz = μ*(vz+dt*k3vz)*sqrt((vx+dt*k3vx)*(vx+dt*k3vx)+(vy+dt*k3vy)*(vy+dt*k3vy)+(vz+dt*k3vz)*(vz+dt*k3vz)) + g;
+    dual k4x = dx(vx+dt*k3vx);
+    dual k4y = dy(vy+dt*k3vy);
+    dual k4z = dz(vz+dt*k3vz);
+    dual k4vx = dvx(vx+dt*k3vx, vy+dt*k3vy, vz+dt*k3vz);
+    dual k4vy = dvy(vx+dt*k3vx, vy+dt*k3vy, vz+dt*k3vz);
+    dual k4vz = dvz(vx+dt*k3vx, vy+dt*k3vy, vz+dt*k3vz);
 
     x += dt*(k1x+2*k2x+2*k3x+k4x)/6;
     y += dt*(k1y+2*k2y+2*k3y+k4y)/6;
@@ -76,7 +112,10 @@ std::pair<dual, dual> getBestShot(dual tx, dual ty, dual rvx, dual rvy, dual θ,
 
 int main()
 {
-  // test for 5,5,1,-5,0,10
-  std::pair<dual, dual> bestShot = getBestShot(5, 5, 1, -5, 0, 10);
-  std::cout << "θ: " << bestShot.first << " v: " << bestShot.second << std::endl;
+  // test time the shot error for 5,5,1,-5,0,10
+  auto start = std::chrono::high_resolution_clock::now();
+  std::cout << "error: " << getShotError(5, 5, 1, -5, 0, 10) << std::endl;
+  auto end = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+  std::cout << "time: " << duration.count() << " microseconds" << std::endl;
 }
